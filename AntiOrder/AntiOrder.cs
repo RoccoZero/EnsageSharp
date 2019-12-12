015a252cc725d433888d73dc421b7c29a54650a0
YEEEEEEE777 <33149580+YEEEEEEE777@users.noreply.github.com>
2019-12-13 01:50:40 +0300

    **Added AntiOrder**

---
+++ AntiOrder/AntiOrder.cs
@@ -0,0 +1,22 @@
+﻿using Ensage;
+
+namespace AntiOrder
+{
+    internal sealed class AntiOrder
+    {
+        public static void Main()
+        {
+            Player.OnExecuteOrder += OnExecuteOrder;
+        }
+
+        private static void OnExecuteOrder(Player sender, ExecuteOrderEventArgs args)
+        {
+            if (args.IsPlayerInput)
+            {
+                return;
+            }
+
+            args.Process = false;
+        }
+    }
+}
