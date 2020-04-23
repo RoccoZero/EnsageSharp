091d052b248232a6d3b928b477d5b99143743c7e
YEEEEEEE777 <33149580+YEEEEEEE777@users.noreply.github.com>
2020-04-23 17:37:50 +0300

    Ensage.HumanizerDisable

---
+++ Ensage.HumanizerDisable/Bootstrap.cs
@@ -0,0 +1,10 @@
+﻿namespace Ensage.HumanizerDisable
+{
+    internal static class Bootstrap
+    {
+        private static void Main()
+        {
+            Config.HumanizerEnabled = false;
+        }
+    }
+}
